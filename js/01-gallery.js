bef4eb294a3ea5684f156f186340bce595486871
Zendr <520530ol@ukr.net>
2023-03-31 15:54:11 +0300

    виконання роботи

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,43 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+
+const galleryIt = document.querySelector('.gallery');
+
+const gallerySelector = (images) => {
+    return images
+        .map(({ preview, original, description }) => {
+            return `<div class = "gallery__item"><a class = "gallery__link" href = ${original}><img class = "gallery__image" src = "${preview}" data-source = ${original} alt = "${description}"></div>`;
+        })
+        .join('');
+};
+
+const imagesMarkup = gallerySelector(galleryItems);
+
+galleryIt.insertAdjacentHTML('afterbegin', imagesMarkup);
+galleryIt.addEventListener('click', gallerySelect);
+
+function gallerySelect(event) {
+    event.preventDefault();
+
+    if (event.target.nodeName !== 'IMG') {
+        return;
+    }
+
+    const instance = basicLightbox.create(
+        `<img src = "${event.target.dataset.source}" width = "800" height = "600">`
+    );
+    instance.show();
+    window.addEventListener('keydown', onCloseModalClick);
+}
+    
+function onCloseModalClick(event) {
+    const openInstance = document.querySelector(".basicLightbox")
+    if (event.code === "Escape") {
+        openInstance.remove();
+           window.removeEventListener('keydown', onCloseModalClick);  
+    }
+
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,22 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+
+function galleryItemEl(arr) {
+  arr.map((item) => {
+      galleryEl.insertAdjacentHTML('afterbegin',
+          `<a class="gallery__item" href='${item.original}'>
+  <img class="gallery__image" src='${item.preview}' alt='${item.description}' />
+</a>`);
+  });
+}
+
+galleryItemEl(galleryItems);
+
+var lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+});
